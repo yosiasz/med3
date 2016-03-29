8c41d8fb9794b39e1188d62bda3c163c9f30e919
Josiah Solomon <Josiah.Solomon@corbis.com>
2016-03-29 09:11:10 -0700

    clean up app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 var express = require('express');
@@ -13,8 +12,6 @@ var port = process.env.PORT || 5000;
 app.use(express.static('./public'));
 app.use(express.static('./src'))
 
-//for basic html
-//app.use(express.static('src/views'));
 
 //templating engine
 app.set('views', './src/views');      
@@ -31,38 +28,3 @@ app.get('/', function (req, res) {
 
 app.listen(port, function () {
     console.log('running server on port ' + port);
-=======
-'use strict';
-
-var express = require('express');
-var app = express();
-
-var saleRouter = require('./src/routes/saleRoutes');
-
-var port = process.env.PORT || 5000;
-
-
-//used by express first
-app.use(express.static('./public'));
-app.use(express.static('./src'))
-
-//for basic html
-//app.use(express.static('src/views'));
-
-//templating engine
-app.set('views', './src/views');      
-app.set('view engine', 'ejs');
-
-
-app.use('/sales', saleRouter.getSales());
-
-app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Hello from render'
-    });
-});
-
-app.listen(port, function () {
-    console.log('running server on port ' + port);
->>>>>>> origin/master
-});
